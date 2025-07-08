c4e9fea793ebe6b566bd4606befed604a8272626
EduardaBedetti <bedettieduarda29@gmail.com>
2025-07-08 13:25:06 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+alert("Boas vindas ao jogo do numero scereto");
+let numeroSecreto = 4;
+console.log(numeroSecreto)
+let chute = prompt("Escolha um número entre 1  e 10");
+
+
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert("Você errou!")
+}
+
+
+
+
+                                                    
