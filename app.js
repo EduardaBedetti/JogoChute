1625c5f1faf74870c994f9b9afaf908fc04bff03
Bedetti <bedettieduarda29@gmail.com>
2025-07-14 14:57:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 alert("Boas vindas ao jogo do numero scereto");
 let numeroSecreto = 4;
-console.log(numeroSecreto)
-let chute = prompt("Escolha um número entre 1  e 10");
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+
+//enquanto o chute nao for igual
+while (chute != numeroSecreto) {
+    chute  = prompt("Escolha um número entre 1  e 10");
+    //se o chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas}`);
     } else {
-        alert(`O número secreto é menor que ${chute}`)
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
     }
 }
 
