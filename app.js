9f7a04e5200b9f68d4339d92079e927a8cf93f13
Bedetti <bedettieduarda29@gmail.com>
2025-07-15 16:01:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-alert("Boas vindas ao jogo do numero scereto");
-let numeroSecreto = 4;
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
-
-
-
-//enquanto o chute nao for igual
-while (chute != numeroSecreto) {
-    chute  = prompt("Escolha um número entre 1  e 10");
-    //se o chute for igual ao numero secreto
-    if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas}`);
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é menor que ${chute}`);
-        }
-        //tentativas = tentativas + 1;
-        tentativas++;
-    }
+//questão 1 corrigida
+diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
 }
 
 
+//questao 2
+let NumeroDigitado = prompt("Digite um numero");
 
+if (NumeroDigitado >= 0) {
+    alert("Positivo");
+}   else{
+    alert("numero negativo");
+}
+
+//questão 3
+let pontuacao = 50;
+
+if (pontuacao >= 100) {
+    console.log("Parabéns você venceu!");
+}   else {
+    console.log("Tente novamente para ganhar");
+}
+
+//questão 4
+let saldoConta = 500; 
+alert(`Seu saldo é de R$${saldoConta}.`);
+
+//questão 5
+let Nome = prompt("Qual o seu nome?");
+alert(`Boas-vindas ${Nome}`);
+
+//Desafios utilizando While.
+
+//questão 1
+let contador = 1;
+while (contador <= 9) {
+    contador += 1;
+}
+
+//questão 2
+let numero = 10;
+while (numero > 0) {
+    numero -= 1;
+}
+
+//questão 3
+let numeroDigitado;
+let contagem;
+
+while (contagem > 0) {
+    numeroDigitado = prompt("Digite um numero")
+   contagem =  Number(numeroDigitado);
+
+   if (contagem > 0)
+    contagem -= 1;
+}
 
                                                     
