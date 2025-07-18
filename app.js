932e88e5fba1e17727eed9ec03cf9a527475cbae
Bedetti <bedettieduarda29@gmail.com>
2025-07-18 15:29:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert("Boas vindas ao jogo do numero scereto");
-let numeroSecreto = 4;
+let numeroSecreto = parseInt(Math.tandom() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -8,10 +8,10 @@ let tentativas = 1;
 
 //enquanto o chute nao for igual
 while (chute != numeroSecreto) {
-    chute  = prompt("Escolha um número entre 1  e 10");
+    chute  = prompt("Escolha um número entre 1  e 100");
     //se o chute for igual ao numero secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas}`);
+       break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -21,3 +21,7 @@ while (chute != numeroSecreto) {
         //tentativas = tentativas + 1;
         tentativas++;
     }
+}
+
+let palavraTentaiva = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentaiva}`);
